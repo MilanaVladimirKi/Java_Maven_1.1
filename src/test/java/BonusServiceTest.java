0fda28121f3e898c500da2292f62fc70d9d71512
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-05-22 22:12:26 +0500

    Border

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -52,4 +52,55 @@ public class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndBorder1Limit() {
+        BonusService service = new BonusService();
+
+        long amount = 16700;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndBorder2Limit() {
+        BonusService service = new BonusService();
+
+        long amount = 16666;
+        boolean registered = true;
+        long expected = 499;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndBorder1Limit() {
+        BonusService service = new BonusService();
+
+        long amount = 49900;
+        boolean registered = false;
+        long expected = 499;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndBorder2Limit() {
+        BonusService service = new BonusService();
+
+        long amount = 50100;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
 }
